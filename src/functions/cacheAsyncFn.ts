6e965f71ba6606c212403f1d5040aa4582aee4d3
Prokop <prokop.schield@protonmail.com>
2023-12-03 15:55:27 +0100

    Release: v0.1.26

--- src/functions/cacheAsyncFn.ts
+++ src/functions/cacheAsyncFn.ts
@@ -2,17 +2,31 @@ export function cacheAsyncFn<X, Y>(
 	fn: (x: X) => Y | Promise<Y>,
 	thisArg?: any
 ): (x: X) => Promise<Y> {
-	const cache = new Map<X, Y>();
+	const cache = new Map<X, Y | Promise<Y>>();
 	const cfn = async (x: X): Promise<Y> => {
 		const cache_val = cache.get(x);
 		if (cache_val !== undefined) {
 			return cache_val;
 		} else {
-			const y = await fn.call(thisArg, x);
-			if (y !== undefined) {
-				cache.set(x, y);
+			const promise = fn.call(thisArg, x);
+
+			cache.set(x, promise);
+
+			try {
+				const y = await promise;
+
+				if (y !== undefined) {
+					cache.set(x, y);
+				}
+
+				return y;
+			} catch (error) {
+				if (cache.get(x) === promise) {
+					cache.delete(x);
+				}
+
+				throw error;
 			}
-			return y;
 		}
 	};
 	return cfn;
