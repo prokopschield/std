f5eaab076e6c6b522a9e865595840f41ded473d2
Prokop <prokop.schield@protonmail.com>
2024-08-23 20:13:30 +0200

    fix(src/functions/sanitizeHeaders.ts): allow digits in header names

--- src/functions/sanitizeHeaders.ts
+++ src/functions/sanitizeHeaders.ts
@@ -8,7 +8,7 @@
 export function sanitizeHeaders(
 	record: Record<string, any>,
 	replacementCharacter = '-',
-	filter_regexp = /[^a-z]+/g
+	filter_regexp = /[^a-z\d]+/g
 ): Record<string, string> {
 	const out: Record<string, string> = {};
 
