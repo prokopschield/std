2d5d054c139d1433c0616cafdb8fd32821cb1875
Prokop <prokop.schield@protonmail.com>
2024-10-26 23:41:51 +0200

    feat(src/classes/Fallible.ts): FallibleSync()

--- src/classes/Fallible.ts
+++ src/classes/Fallible.ts
@@ -1,20 +1,48 @@
-import { FutureResult } from './Result';
+import { FutureResult, Result } from './Result';
 
-export type Fallible<A extends any[], O = undefined> = (
+export type FallibleSync<A extends any[], O = undefined> = (
+	...args: A
+) => Result<O>;
+
+export type FallibleAsync<A extends any[], O = undefined> = (
 	...args: A
 ) => FutureResult<O>;
 
-export function Fallible<A extends any[], O>(
-	callback: (..._arguments: A) => O | PromiseLike<O>
-): Fallible<A, O> {
-	return function Fallible(...args: A) {
+export type Fallible<A extends any[], O = undefined> =
+	| FallibleSync<A, O>
+	| FallibleAsync<A, O>;
+
+export function FallibleSync<A extends any[], O>(
+	callback: (...args: A) => O
+): FallibleSync<A, O> {
+	const fallible = function Fallible(...args: A): Result<O> {
+		return Result.try(() => callback(...args));
+	};
+
+	Object.setPrototypeOf(fallible, FallibleSync);
+
+	return fallible;
+}
+
+export function FallibleAsync<A extends any[], O>(
+	callback: (...args: A) => O | PromiseLike<O>
+): FallibleAsync<A, O> {
+	const fallible = function Fallible(...args: A): FutureResult<O> {
 		return new FutureResult(() => callback(...args));
 	};
+
+	Object.setPrototypeOf(fallible, FallibleAsync);
+
+	return fallible;
 }
+
+export const Fallible = FallibleAsync;
 
 export default Fallible;
 
 Object.defineProperties(Fallible, {
 	default: { get: () => Fallible },
 	Fallible: { get: () => Fallible },
+	FallibleSync: { get: () => FallibleSync },
+	FallibleAsync: { get: () => FallibleAsync },
 });
