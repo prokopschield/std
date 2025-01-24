9ae2ba188fd4f316cb0ec6e5250c8f6c84bb4928
Prokop <prokop.schield@protonmail.com>
2025-01-24 16:34:54 +0100

    refactor: use symbol to represent undefined value

--- src/functions/once.ts
+++ src/functions/once.ts
@@ -1,12 +1,14 @@
+export const symbol = Symbol.for('一度');
+
 export function once<T>(fn: () => T, thisArg?: any): () => T {
-	let value: T | undefined = undefined;
+	let value: T | typeof symbol = symbol;
 
 	const cfn = (): T => {
-		if (value === undefined) {
+		if (value === symbol) {
 			value = fn.call(thisArg);
 		}
 
-		return value!;
+		return value as T;
 	};
 
 	return cfn;
